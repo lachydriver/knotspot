2431c33c01418b5a9dbccf9580b32080a75801e4
Lachlan Driver <lachydriver@gmail.com>
2019-08-02 18:07:54 +1000

    added react-router, worked on splash page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Start from './components/start';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 import './styles.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <div className="container">
-      <Start/>
+        <Switch>
+        <Route path="/" exact component={Start}/>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
