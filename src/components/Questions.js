5b3eabd5dba582d0acc7ad5faa664ac4892222ff
Lachlan Driver <lachydriver@gmail.com>
2019-08-08 20:01:01 +1000

    started development on questions component

---
+++ src/components/Questions.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class Questions extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            question_id: 0
+        };
+        this.renderQuestion = this.renderQuestion.bind(this);
+        this.nextQuestion = this.nextQuestion.bind(this);
+    }
+
+    componentDidMount(){
+        this.setState({question_id: 1})
+    }
+
+    renderQuestion(){
+        if(this.state.question_id === 1){
+            return('Where are you experiencing pain?')
+        }
+        if(this.state.question_id === 2){
+            return('Question 2')
+        }
+    }
+
+    nextQuestion(){
+        this.setState(prevstate => {return {question_id: prevstate.question_id + 1}})
+    }
+
+    render(){
+        return(
+            <div>
+                <p>state: {this.state.question_id}</p>
+                {this.renderQuestion()}<br/>
+                <button onClick={this.nextQuestion}>Next</button>
+            </div>
+        )
+    }
+
+}
+
+export default Questions;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,6 +3,7 @@ import Helmet from "react-helmet";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { updateSum } from "../store/index";
+import Questions from './Questions';
 
 class Quiz extends React.Component {
   constructor(props) {
@@ -20,8 +21,8 @@ class Quiz extends React.Component {
 
   render() {
     return (
-      <div>
-       {<ul className="list-group list-group-flush">
+      <div className="quiz">
+{/*        {<ul className="list-group list-group-flush">
           {this.props.bones.map(el => (
             <li className="list-group-item" key={el.id}>
               {el.name} - {el.sum}
@@ -30,7 +31,13 @@ class Quiz extends React.Component {
         </ul>}
         <button onClick={() => this.updateSumSubmit(0, 5)}>Add 5 to Scalene</button>
         <button onClick={() => this.updateSumSubmit(1, 3)}>Add 3 to Para</button>
-        <button onClick={() => this.updateSumSubmit(2, 1)}>Add 1 to Neck</button>
+        <button onClick={() => this.updateSumSubmit(2, 1)}>Add 1 to Neck</button> */}
+        
+        <Helmet>
+          <title>MSDS</title>
+        </Helmet>
+        <h1>Quiz</h1>
+        <Questions/>
       </div>
     );
   }
