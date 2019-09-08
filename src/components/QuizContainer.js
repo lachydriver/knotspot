8314a1415c51881c4ad916ca5837057ec5090c9a
Lachlan Driver <lachydriver@gmail.com>
2019-09-08 18:18:43 +1000

    added profile/login button to quiz

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -1,10 +1,30 @@
 import React from "react";
 import Quiz from "./Quiz";
+import {Link} from "react-router-dom";
+import {connect} from 'react-redux';
 
 class QuizContainer extends React.Component {
+
+  displayButton = () => {
+    if(this.props.auth.isAuthenticated === true){
+    return(<Link to="/profile" className="profile-button retakebutton">
+      Profile
+    </Link>)
+    } else {
+    return(<Link to="/login" className="profile-button retakebutton">
+      Login
+    </Link>)
+    }
+  }
+
+  componentDidMount(){
+    
+  }
+
   render() {
     return (
       <div className="page-container">
+        {this.displayButton()}
         <div className="container">
           <Quiz />
         </div>
@@ -13,4 +33,10 @@ class QuizContainer extends React.Component {
   }
 }
 
-export default QuizContainer;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps
+)(QuizContainer)
