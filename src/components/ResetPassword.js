38a3470fbb531bc218771ad1b6b1fe9d416e9e7c
Lachlan Driver <lachydriver@gmail.com>
2019-09-22 19:19:59 +1000

    Update ResetPassword.js

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -11,7 +11,8 @@ class ResetPassword extends Component {
       error: false,
       username: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      passworderror: ""
     };
   }
 
@@ -33,56 +34,113 @@ class ResetPassword extends Component {
     });
   }
 
-  render() {
-    return (
-      <div className="form-root">
-        <Helmet>
-          <title>Knot Spot - Reset Password</title>
-        </Helmet>
+  resetPassword = (e) => {
+      e.preventDefault()
+      if(this.state.password != this.state.confirmPassword){
+          this.setState({passworderror: "Passwords do not match"})
+      } else {
+        this.setState({passworderror: "Success"})
+      }
+  }
 
-        <Link to="/" className="profile-button retakebutton">
-          Home
-        </Link>
-        <div className="form">
-          <div className="PageSwitcher">
-            <NavLink
-              to="/login"
-              activeClassName="PageSwitcher__Item--Active"
-              className="PageSwitcher__Item"
-            >
-              Back
-            </NavLink>
-          </div>
-          <div className="login">
-            <h1 className="titleforgot">Reset Password</h1>
-            <form className="FormFields" noValidate>
-              <div className="inputgrp">
-                <div className="inputs">
-                  <label className="labels" htmlFor="email">
-                    Email
-                  </label>
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    onChange={this.handleChange}
-                  />
-                  <br />
+  handleChange = e => {
+    this.setState({ password: e.target.value });
+  };
+  handleConfirmChange = e => {
+    this.setState({ confirmPassword: e.target.value });
+  };
+
+  render() {
+    const { error } = this.state;
+    if(error){
+        return(
+            <div className="form-root">
+              <Helmet>
+                <title>Knot Spot - Reset Password</title>
+              </Helmet>
+      
+              <Link to="/" className="profile-button retakebutton">
+                Home
+              </Link>
+              <div className="form">
+                <div className="PageSwitcher">
+                  <NavLink
+                    to="/login"
+                    activeClassName="PageSwitcher__Item--Active"
+                    className="PageSwitcher__Item"
+                  >
+                    Back
+                  </NavLink>
+                </div>
+                <div className="login">
+                  <h1 className="titleforgot">This token is invalid or has expired.</h1>
                 </div>
               </div>
-
-              <button
-                type="submit"
-                className="login-btn"
-                onClick={this.sendMail}
-              >
-                Forgot Password
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
+            </div>
+        )
+    } else {
+        return (
+            <div className="form-root">
+              <Helmet>
+                <title>Knot Spot - Reset Password</title>
+              </Helmet>
+      
+              <Link to="/" className="profile-button retakebutton">
+                Home
+              </Link>
+              <div className="form">
+                <div className="PageSwitcher">
+                  <NavLink
+                    to="/login"
+                    activeClassName="PageSwitcher__Item--Active"
+                    className="PageSwitcher__Item"
+                  >
+                    Back
+                  </NavLink>
+                </div>
+                <div className="login">
+                  <h1 className="titleforgot">Reset Password</h1>
+                  <p>Username: {this.state.username}</p>
+                  <form className="FormFields" noValidate>
+                    <div className="inputgrp">
+                      <div className="inputs">
+                        <label className="labels" htmlFor="password">
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          name="password"
+                          id="password"
+                          onChange={this.handleChange}
+                        />
+                        <br />
+                        <label className="labels" htmlFor="password">
+                          Confirm Password
+                        </label>
+                        <input
+                          type="password"
+                          name="confirmpassword"
+                          id="confirmpassword"
+                          onChange={this.handleConfirmChange}
+                        />
+                        <br />
+                        {this.state.passworderror}
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="login-btn"
+                      onClick={this.resetPassword}
+                    >
+                      Forgot Password
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          );
+    }
+    
   }
 }
 
